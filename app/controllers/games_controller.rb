88d2b89ca95e433a96de84a4b0b062ef3d72c909
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2019-12-17 23:36:33 +0000

    updated new form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,10 +2,11 @@ class GamesController < ApplicationController
 
 
   def new
-    @letters = Array.new(10) { ('A'..'Z').sample }
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
     @letters.shuffle!
   end
 
   def score
+    raise
   end
 end
