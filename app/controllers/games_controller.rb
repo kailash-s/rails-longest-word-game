3e8cf522144db105212515ff1b3773bf3c4080ea
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2019-12-18 01:46:33 +0000

    updated scores

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,27 @@
+require "open-uri"
+
 class GamesController < ApplicationController
-
-
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
     @letters.shuffle!
   end
 
   def score
-    raise
+    @letters = params[:letters].split
+    @word = (params[:word] || '').upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
